d2ea6d3a96c9a31c0b891dc1e51d8c6e071ee4b1
anmolnagpal18 <anmoln507@gmail.com>
2026-07-26 10:47:31 +0530

    fix: Correct settings TIME_ZONE to Asia/Kolkata for local timezone alignment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -154,7 +154,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
